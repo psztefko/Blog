0942bfcd43b0f31384b258c837dbdd409485ad0b
psztefko <57404599+psztefko@users.noreply.github.com>
2021-03-18 21:22:57 +0100

    Update settings.py

--- TI_project/settings.py
+++ TI_project/settings.py
@@ -136,6 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "piotr.sztefko@edu.uekat.pl"
-EMAIL_HOST_PASSWORD = "otuvodkhlylkirvz"
-
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
